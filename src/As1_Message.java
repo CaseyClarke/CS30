b43f8ffe3a6ec631d471f61f126925f68c1406eb
Casey-Clarke <thisisforvancedonly@gmail.com>
2024-04-09 19:18:29 -0600

    Removed useless Getter/Setter functions

--- src/As1_Message.java
+++ src/As1_Message.java
@@ -1,9 +1,9 @@
 import java.util.*;
 
 public class As1_Message {
-    private String machineType;
-    private String machineID;
-    private String warning;
+    private final String machineType;
+    private final String machineID;
+    private final String warning;
 
     As1_Message(String fullMessage) {
         String[] messageParts = fullMessage.split(":");
@@ -19,32 +19,7 @@ public class As1_Message {
         return Arrays.asList(warningParts).contains(keyword);
     }
 
-    public String getMachineType() {
-        return machineType;
-    }
-
-    public void setMachineType(String machineType) {
-        this.machineType = machineType;
-    }
-
-    public String getMachineID() {
-        return machineID;
-    }
-
-    public void setMachineID(String machineID) {
-        this.machineID = machineID;
-    }
-
-    public String getWarning() {
-        return warning;
-    }
-
-    public void setWarning(String warning) {
-        this.warning = warning;
-    }
-
-    @Override
-    public String toString() {
+    @Override public String toString() {
         return "As1_Message{" +
                 "machineType='" + machineType + '\'' +
                 ", machineID='" + machineID + '\'' +
