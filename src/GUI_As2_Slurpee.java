045bf8b98224b2c514386d382e9d8c2cbe450b10
Casey-Clarke <thisisforvancedonly@gmail.com>
2024-05-02 14:30:05 -0600

    Finished 2nd GUI assignment

--- src/GUI_As2_Slurpee.java
+++ src/GUI_As2_Slurpee.java
@@ -1,9 +1,37 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.*;
 
 public class GUI_As2_Slurpee {
     private JPanel mainPanel;
     private JLabel slurpee;
+    private JComboBox flavorBox;
+    private JComboBox sizeBox;
+    private JButton purchaseButton;
+    private JTextArea output;
+
+    public GUI_As2_Slurpee() {
+        HashMap<String, Double> sizes = new HashMap<String, Double>();
+        String[] flavors = {"Cream soda", "Crush", "Orange", "Mystery"};
+        {
+            sizes.put("Small", 1.25);
+            sizes.put("Medium", 1.50);
+            sizes.put("Large", 1.75);
+            sizes.put("Extra Large", 2.00);
+        }
+
+        flavorBox.setModel(new javax.swing.DefaultComboBoxModel(flavors));
+        sizeBox.setModel(new javax.swing.DefaultComboBoxModel(sizes.keySet().toArray(new String[0])));
+
+        purchaseButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                output.append(sizeBox.getSelectedItem() + " " + flavorBox.getSelectedItem() + " " + sizes.get(sizeBox.getSelectedItem())  +  "$ \n");
+            }
+        });
+    }
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("GUI_As2_Slurpee");
@@ -15,7 +43,7 @@ public class GUI_As2_Slurpee {
 
     private void createUIComponents() {
         slurpee = new JLabel();
-        ImageIcon imageIcon = new ImageIcon(new ImageIcon("F:\\IntelliJ_Java_D1B2\\CS30\\src\\slurpees.jpg").getImage().getScaledInstance(200, 200, Image.SCALE_SMOOTH));
+        ImageIcon imageIcon = new ImageIcon(new ImageIcon("src/slurpee.jpg").getImage().getScaledInstance(200, 200, Image.SCALE_SMOOTH));
         slurpee.setIcon(imageIcon);
     }
 }
